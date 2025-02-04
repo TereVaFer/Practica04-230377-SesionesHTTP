02e13a84e623a84f8da2d6c63ef0ae017ab855ae
Teresa Vargas Fernandez <230377@utxicotepec.edu.mx>
2025-02-04 08:55:02 -0600

    Practica 4
    Pruebas

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ import moment from 'moment-timezone';
 import cors from 'cors';
 import { v4 as uuid4 } from 'uuid';
 import os from 'os';
+import morgan from "morgan";
 
 const app = express();
 const PORT = 3000;
 const sessions = {};
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -27,32 +28,240 @@ app.use(
 );
 
 // Función de utilidad que permite acceder a la IP del cliente
-const getClientIp = (req) => {
-    return (
-        req.headers["x-forwarded-for"] ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket?.remoteAddress
-    );
-};
+// const getClientIp = (req) => {
+   // return (
+    //    req.headers["x-forwarded-for"] ||
+      //  req.connection.remoteAddress ||
+      //  req.socket.remoteAddress ||
+      //  req.connection.socket?.remoteAddress
+    //);
+//};
 
 // Función de utilidad que obtiene la IP local
-const getLocalIp = () => {
-    const networkInterfaces = os.networkInterfaces();
-    for (const interfaceName in networkInterfaces) {
-        const interfaces = networkInterfaces[interfaceName];
-        for (const iface of interfaces) {
-            // IPv4 y no interna (no localhost)
-            if (iface.family === "IPv4" && !iface.internal) {
-                return iface.address;
+const getClientIP = (req) => {
+    const ip = req.headers["x-forwarded-for"]?.split(",")[0]?.trim() ||
+               req.connection?.remoteAddress ||
+               req.socket?.remoteAddress ||
+               req.connection?.socket?.remoteAddress ||
+               "IP no disponible";
+
+    if (ip.startsWith("::ffff:")) {
+        return ip.slice(7); 
+    }
+
+    return ip;
+};
+
+
+const getServerNetworkInfo = () => {
+    const interfaces = os.networkInterfaces();
+    for(const name in interfaces){
+        for(const iface of interfaces[name]){
+            if(iface.family === 'IPv4' && !iface.internal){
+                return {
+                    serverIp: iface.address,
+                    serverMac: iface.mac,
+                }
             }
         }
     }
-    return null; // Retorna null si no encuentra una IP válida
-};
+}
+
+
+app.post("/login", (req, res) => {
+    const { nickname, email, macAddress } = req.body;
+    const serverInfo = getServerNetworkInfo();
+
+    if (!email || !nickname || !macAddress) {
+        return res.status(400).json({
+            message: "Se esperan campos requeridos",
+        });
+    }
+
+    const sessionId = uuidv4();
+    const now = new Date();
+    const horaMexico = moment(now).tz("America/Mexico_City").format("YYYY-MM-DD HH:mm:ss")
+    const ipClient = getClientIP(req)
+
+    sessions[sessionId] = {
+        sessionId,
+        email,
+        nickname,
+        macAddress,
+        ip: ipClient,
+        dateCreated: horaMexico, 
+        lastAccessed: horaMexico,
+        duration: 0,
+        inactivityTime: 0,
+        ipServer: serverInfo.serverIp,
+        macServer: serverInfo.serverMac,
+        status: true
+    };
+
+    res.status(200).json({
+        message: "Se ha logueado con éxito",
+        sessionId,
+    });
+});
+
+app.post("/logout", (req, res) => {
+    const { sessionId } = req.body;
+
+    if (!sessionId || !sessions[sessionId]) {
+        return res.status(404).json({
+            message: "No se ha encontrado una sesión activa",
+        });
+    }
+
+    delete sessions[sessionId];
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).send("Error al cerrar la sesión");
+        }
+    });
+
+
+    res.status(200).json({
+        message: "Logout successful",
+    });
+});
+
+app.put("/update", (req, res) => {
+    const { sessionId, email, nickname } = req.body;
+
+    // Validar si la sesión existe antes de acceder a sus propiedades
+    if (!sessionId || !sessions[sessionId]) {
+        return res.status(404).json({
+            message: "No existe una sesión activa"
+        });
+    }
+
+    const now = new Date();
+    const horaMexico = moment(now).tz("America/Mexico_City").format("YYYY-MM-DD HH:mm:ss");
+    
+    // Obtener datos de la sesión después de la validación
+    const duration = new Date(horaMexico) - new Date(sessions[sessionId].dateCreated);
+    const inactivity = new Date(horaMexico) - new Date(sessions[sessionId].lastAccessed);
+
+    console.log(`sessionID: ${sessionId} email: ${email} nickname: ${nickname}`);
+
+    // Actualizar los campos proporcionados
+    if (email) sessions[sessionId].email = email;
+    if (nickname) sessions[sessionId].nickname = nickname;
+    sessions[sessionId].lastAccessed = horaMexico;
+    sessions[sessionId].duration = duration;
+    sessions[sessionId].inactivityTime = inactivity;
+
+    // Responder con el estado actualizado de la sesión
+    res.status(200).json({
+        message: "Sesión actualizada exitosamente",
+        session: sessions[sessionId]
+    });
+});
+
+app.get("/status", (req, res) => {
+    const sessionId = req.query.sessionId;
+
+    if (!sessionId || !sessions[sessionId]) {
+        return res.status(404).json({
+            message: "No hay sesión activa"
+        });
+    }
+
+    const now = new Date();
+    const horaMexico = moment(now).tz("America/Mexico_City").format("YYYY-MM-DD HH:mm:ss");
+
+    const duration = new Date(horaMexico) - new Date(sessions[sessionId].dateCreated);
+    const inactivity = new Date(horaMexico) - new Date(sessions[sessionId].lastAccessed);
+
+    sessions[sessionId].lastAccessed = horaMexico;
+    sessions[sessionId].duration = duration;
+    sessions[sessionId].inactivityTime = inactivity;
+
+    res.status(200).json({
+        message: "Sesión activa",
+        session: sessions[sessionId]
+    });
+});
+
+
+app.get("/statusAllActives", (req, res) => {
+    if (!sessions || Object.keys(sessions).length === 0) {
+        return res.status(404).json({
+            message: "No hay sesiones activas"
+        });
+    }
+
+    const activeSessions = Object.entries(sessions).map(([sessionId, sessionData]) => ({
+        sessionId,
+        sessionData
+    }));
+
+    res.status(200).json({
+        message: "Listado de sesiones activas",
+        activeSessions
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor levantado en el puerto ${PORT}`);
+});
+
+app.get("/", (req, res)=>{
+    return res.status(200).json({
+        message:"Bienvenid@ a mi API de control de Sesiones",
+        author:"Teresa Vargas Fernández"
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Bienvenida a la API
-app.get("/welcome", (req, res) => {
+/*app.get("/welcome", (req, res) => {
     return res.status(200).json({
         message: "Bienvenido a la API de Control de Sesion.",
         author: "Teresa Vargas Fernández",
@@ -148,4 +357,4 @@ app.get("/status", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
-});
+});*/
